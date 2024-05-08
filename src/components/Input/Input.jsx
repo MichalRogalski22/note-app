f4b8c463c23e325ea6aa49e2b04d4afb349c6b23
MichalRogalski22 <michalrogalsk@gmail.com>
2024-05-08 21:24:51 +0200

    Create separate component from input

---
+++ src/components/Input/Input.jsx
@@ -0,0 +1,14 @@
+import s from "./style.module.css";
+
+const Input = ({ onTextChange, placeholder, type }) => {
+  return (
+    <input
+      type={type || "text"}
+      className={s.input}
+      onChange={(e) => onTextChange(e.target.value)}
+      placeholder={placeholder}
+    />
+  );
+};
+
+export default Input;
